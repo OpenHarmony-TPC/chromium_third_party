b9c33ebbdcaa511bfd637dff9002a911aace454a
yangguangzhao <yangguangzhao1@huawei.com>
2023-09-15 15:34:38 +0800

    scroll focused node into view only when keyboard show/hide
    Signed-off-by: yangguangzhao <yangguangzhao1@huawei.com>

--- blink/renderer/core/frame/web_frame_widget_impl.cc
+++ blink/renderer/core/frame/web_frame_widget_impl.cc
@@ -1478,12 +1478,8 @@ void WebFrameWidgetImpl::UpdateVisualProperties(
   // when the focused node is inside an OOPIF. This code path where
   // scroll_focused_node_into_view is set is used only for WebView, crbug
   // 939118 tracks fixing webviews to not use scroll_focused_node_into_view.
-#if BUILDFLAG(IS_OHOS)
-  ScrollFocusedEditableElementIntoView();
-#else
   if (visual_properties.scroll_focused_node_into_view)
     ScrollFocusedEditableElementIntoView();
-#endif
 }
 
 void WebFrameWidgetImpl::ApplyVisualPropertiesSizing(
