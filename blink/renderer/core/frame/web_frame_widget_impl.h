c9ecd80dd239ac68837f7dbc9308960d8cc36184
YJone <zhouyongqiang8@huawei.com>
2024-09-26 12:17:07 +0000

    update blink/renderer/core/frame/web_frame_widget_impl.h.
    Signed-off-by: YJone <zhouyongqiang8@huawei.com>

--- blink/renderer/core/frame/web_frame_widget_impl.h
+++ blink/renderer/core/frame/web_frame_widget_impl.h
@@ -432,6 +432,7 @@ class CORE_EXPORT WebFrameWidgetImpl
   void DidNativeEmbedEvent(const WebPointerEvent& web_pointer_event, std::string embedId,
                         PhysicalOffset& offset, bool isCancel);
   void TouchHitTest(const WebPointerEvent& event, size_t i) override;
+  void DisableFlingBoost();
 #endif
   // WidgetBaseClient overrides:
   void OnCommitRequested() override;
