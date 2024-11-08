3e05f669a1a4d2ad54bb75f88a7153ff1a52c540
wuqiwen <wuqiwen7@huawei.com>
2024-11-08 11:20:25 +0000

    update blink/renderer/platform/fonts/ohos/font_unique_name_lookup_ohos.h.
    Signed-off-by: wuqiwen <wuqiwen7@huawei.com>

--- blink/renderer/platform/fonts/ohos/font_unique_name_lookup_ohos.h
+++ blink/renderer/platform/fonts/ohos/font_unique_name_lookup_ohos.h
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 // Based on font_unique_name_lookup_android.h originally written by
-// Copyright 2018 The Chromium Authors
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
