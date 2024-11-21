40a8c08067fa84ccd4bc71e855b05618081ba207
j60033653 <jianghengbo@h-partners.com>
2024-11-21 19:47:13 +0800

    cherry pick c14a5ab from https://gitee.com/HB-J/chromium_third_party/pulls/774 修复CVE-2024-11110
    Signed-off-by: j60033653 <jianghengbo@h-partners.com>

--- blink/renderer/core/loader/document_loader.cc
+++ blink/renderer/core/loader/document_loader.cc
@@ -3373,7 +3373,10 @@ scoped_refptr<const SharedBuffer> DocumentLoader::OnGetImageFromCache(
   if (kurl.IsValid() && frame_ &&
       GetFrameLoader().GetDocumentLoader() == this) {
     resource = MemoryCache::Get()->ResourceForURL(
-        kurl, frame_->GetDocument()->Fetcher()->GetCacheIdentifier(kurl));
+        kurl,
+        frame_->GetDocument()->Fetcher()->GetCacheIdentifier(
+                                          kurl,
+                                          /*skip_service_worker=*/false));
 
     if (!resource) {
       HeapVector<Member<Resource>> resources =
