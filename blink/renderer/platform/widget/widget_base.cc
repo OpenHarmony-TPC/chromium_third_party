89216c09ed7f959a4cee4bc9a66ca5f83dd5b933
yiye <yiye1@huawei.com>
2024-12-11 09:59:11 +0000

    更新信息
    Signed-off-by: yiye <yiye1@huawei.com>

--- blink/renderer/platform/widget/widget_base.cc
+++ blink/renderer/platform/widget/widget_base.cc
@@ -1022,7 +1022,9 @@ void WidgetBase::UpdateTextInputStateInternal(bool show_virtual_keyboard,
     // Check whether the keyboard should always be hidden for the currently
     // focused element.
     always_hide_ime = frame_widget->ShouldSuppressKeyboardForFocusedElement();
-    LOG(INFO) << "UpdateTextInputStateInternal update always_hide_ime value:" << always_hide_ime;
+    if (always_hide_ime) {
+        LOG(INFO) << "UpdateTextInputStateInternal update always_hide_ime value:" << always_hide_ime;
+    }
     frame_widget->GetEditContextBoundsInWindow(&control_bounds,
                                                &selection_bounds);
     frame_widget->GetInputElementAttributes(input_atrributes);
