4c83365b956623aa8fd89760093b20afe80a2c2f
zhaowenpu <zhaowenpu1@huawei.com>
2024-12-28 10:25:03 +0000

    update blink/renderer/core/html/media/html_media_element.cc.
    Signed-off-by: zhaowenpu <zhaowenpu1@huawei.com>

--- blink/renderer/core/html/media/html_media_element.cc
+++ blink/renderer/core/html/media/html_media_element.cc
@@ -4578,6 +4578,7 @@ void HTMLMediaElement::Trace(Visitor* visitor) const {
   visitor->Trace(error_);
   visitor->Trace(current_source_node_);
   visitor->Trace(next_child_node_to_consider_);
+  visitor->Trace(next_retry_child_node_);
   visitor->Trace(deferred_load_timer_);
   visitor->Trace(media_source_tracer_);
   visitor->Trace(audio_tracks_);
