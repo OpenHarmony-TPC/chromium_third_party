3344a62fef0917a3853a697358d0e2ad9d4bb363
wen-xue-yang <yangwenxue1@huawei.com>
2025-01-13 18:09:41 +0800

    bugfix
    Signed-off-by: wen-xue-yang <yangwenxue1@huawei.com>

--- blink/renderer/core/frame/local_frame_view.cc
+++ blink/renderer/core/frame/local_frame_view.cc
@@ -846,10 +846,10 @@ void LocalFrameView::PerformLayout() {
     int body_height = 0;
     Element* document_element = document->documentElement();
     if (document_element && document_element->GetLayoutObject()) {
-      html_box = To<LayoutBox>(document_element->GetLayoutObject());
+      html_box = DynamicTo<LayoutBox>(document_element->GetLayoutObject());
     }
-    if (html_box && IsA<LayoutBox>(html_box) && html_box->FirstChildBox()) {
-      body_box = html_box->FirstChildBox();
+    if (html_box && IsA<LayoutBox>(html_box) && html_box->SlowFirstChild()) {
+      body_box = DynamicTo<LayoutBox>(html_box->SlowFirstChild());
     }
     if (body_box && IsA<LayoutBox>(body_box) && (body_box->GetLayoutResults().size() > 0)) {
       body_height = body_box->ScrollHeight().ToInt();
