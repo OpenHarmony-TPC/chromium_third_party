836f67301e27454fced50e1cb17177400f468ee0
diao-gaoyang <diaogaoyang@huawei.com>
2025-02-27 17:48:35 +0800

    update: 更新文件 native_loader.h 合入 统一boundingbox计算格式，避免缩放后减法再取证造成的像素进一 同层渲染创建时position缩放
    Signed-off-by: diao-gaoyang <diaogaoyang@huawei.com>

--- blink/renderer/core/loader/native_loader.h
+++ blink/renderer/core/loader/native_loader.h
@@ -146,6 +146,7 @@ class CORE_EXPORT NativeLoader
   std::unique_ptr<WebNativeBridge> web_native_bridge_;
   int native_embed_id_ = -1;
   bool first_update_rect_ = true;
+  bool cc_layer_update_ = false;
 
   Member<DisallowNewWrapper<
       HeapMojoAssociatedRemote<media::mojom::blink::NativeBridgeHost>>>
